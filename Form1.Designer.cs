01798b2eb657bd7660e859daea6b04bca38b42e3
wkong <reg@wk0ng.com>
2023-02-21 10:44:02 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Daily
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "报表";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(298, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "输出";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(58, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(298, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "排除";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(58, 111);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(298, 77);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(58, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 25);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "导出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "总天";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(58, 81);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(44, 21);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(255, 207);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(101, 12);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "有bug联系wkong、";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 230);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(461, 269);
+            this.MinimumSize = new System.Drawing.Size(461, 269);
+            this.Name = "Form1";
+            this.Text = "日报统计";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using MSWord = Microsoft.Office.Interop.Excel;
+
+namespace Daily
+{
+    public partial class Form1 : Form
+    {
+        private string intoFilename = "";
+        private int maxDay = 0;
+        struct Info
+        {
+            public string name;
+            public List<string> days;
+            public List<string> other;
+            public List<string> latter;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择文件";
+            dialog.Filter = "Excel文件(*.xls,*.xlsx)|*.xls;*.xlsx";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.intoFilename = dialog.FileName;
+                this.textBox1.Text = this.intoFilename;
+                this.textBox2.Text = this.textBox1.Text + "____导出.xlsx";
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox3.Text = Regex.Replace(textBox3.Text, @"\D", ",");
+            textBox3.Text = Regex.Replace(textBox3.Text, @",{2,}", ",");
+            textBox3.Select(textBox3.TextLength, 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.button2.Text = "正在统计";
+            this.button2.Enabled = false;
+            this.button1.Enabled = false;
+            this.textBox2.ReadOnly = true;
+            this.textBox3.ReadOnly = true;
+            this.textBox4.ReadOnly = true;
+
+            // 加载不需要填写日期，给1~9前面加0
+            String[] notNeed = textBox3.Text.Split(',');
+
+            for(int i=0; i<notNeed.Length; i++)
+            {
+                if (notNeed[i].Length == 1)
+                {
+                    notNeed[i] = '0' + notNeed[i];
+                }
+            }
+
+            // 加载需要填写日期，给1~9前面加0
+            List<String> needSub = new List<string>();
+            for(int i=1; i<maxDay; i++)
+            {
+                string tmpday = i.ToString();
+                if (i < 10)
+                {
+                    tmpday = "0" + tmpday;
+                }
+
+                if (!notNeed.Contains(tmpday))
+                {
+                    needSub.Add(tmpday);
+                }
+            }
+
+            MSWord.Application excelInput = new MSWord.Application();
+            excelInput.Visible = false;
+            MSWord.Workbooks wbs = excelInput.Workbooks;
+
+            MSWord._Workbook wb = wbs.Add(textBox1.Text);
+
+            Dictionary<string, Info> infoList = new Dictionary<string, Info>();
+            for(int i=1;i < wb.Sheets.Count+1; i++)
+            {
+                MSWord._Worksheet inpuSheet = wb.Sheets[i];
+                int rowCount = inpuSheet.Rows.Count;
+
+                for(int r = 2; r < rowCount + 1; r++)
+                {
+                    String name = ((MSWord.Range)inpuSheet.Cells[r, 2]).Text;
+                    String myDate = ((MSWord.Range)inpuSheet.Cells[r, 4]).Text;
+                    if(name != "" && myDate != "")
+                    {
+                        String day = myDate.Split(new string[] { "年" }, StringSplitOptions.RemoveEmptyEntries)[1].Split(new string[] { "月" }, StringSplitOptions.RemoveEmptyEntries)[1].Split(new string[] { "日" }, StringSplitOptions.RemoveEmptyEntries)[0];
+                        String timeHour = myDate.Split(' ')[1].Split(':')[0];
+                        String timeMin = myDate.Split(' ')[1].Split(':')[1];
+
+                        if (!infoList.ContainsKey(name))
+                        {
+                            Info newUser = new Info();
+                            newUser.name = name;
+                            newUser.days = new List<string>();
+                            newUser.other = new List<string>();
+                            newUser.latter = new List<string>();
+                            infoList.Add(name, newUser);
+                        }
+
+                        infoList[name].days.Add(day);
+
+                        if (notNeed.Contains(day))
+                        {
+                            infoList[name].other.Add(day);
+                        }
+                        else
+                        {
+                            if(timeHour=="23" && timeMin != "00")
+                            {
+
+                                infoList[name].latter.Add(day+"日"+" "+timeHour+":"+timeMin);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        r = rowCount;
+                    }
+                }
+            }
+            //关闭excel
+            wb.Close();
+            wbs.Close();
+            excelInput.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelInput);
+
+            //开始写入excel
+            object Nothing = System.Reflection.Missing.Value;
+
+            MSWord.Application excelOutput = new MSWord.Application();
+            excelOutput.Visible = false;
+            MSWord.Workbooks wbsOut = excelOutput.Workbooks;
+
+            MSWord._Workbook wbOut = wbsOut.Add(Nothing);
+
+            MSWord.Worksheet wbSheet = (MSWord.Worksheet)wbOut.Sheets[1];
+            wbSheet.Name = "日报统计";
+
+            wbSheet.Cells[1, 1] = "序号";
+            wbSheet.Cells[1, 2] = "钉钉昵称";
+            wbSheet.Cells[1, 3] = "提交次数";
+            wbSheet.Cells[1, 4] = "未提交";
+            wbSheet.Cells[1, 5] = "晚交";
+            wbSheet.Cells[1, 6] = "加班确认";
+
+            Dictionary<string, Info>.KeyCollection allKeys = infoList.Keys;
+
+            int tNum = 1;
+            
+            foreach(string k in allKeys)
+            {
+                Info tmp = infoList[k];
+                int total = tmp.days.Count;
+                string notSub = "";
+                string latterSub = string.Join("、", tmp.latter.ToArray());
+                string otherSub = string.Join("、", tmp.other.ToArray());
+
+                for(int i =0;i<needSub.Count; i++)
+                {
+                    if (!tmp.days.Contains(needSub[i]))
+                    {
+                        if (notSub != "")
+                        {
+                            notSub = notSub + "、";
+                        }
+                        notSub = notSub + needSub[i];
+                    }
+                }
+
+                wbSheet.Cells[tNum + 1, 1] = tNum.ToString();
+                wbSheet.Cells[tNum + 1, 2] = tmp.name;
+                wbSheet.Cells[tNum + 1, 3] = total.ToString();
+                wbSheet.Cells[tNum + 1, 4] = notSub;
+                wbSheet.Cells[tNum + 1, 5] = latterSub;
+                wbSheet.Cells[tNum + 1, 6] = otherSub;
+
+                tNum = tNum + 1;
+            }
+
+            wbSheet.SaveAs(textBox2.Text, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, MSWord.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing);
+
+
+            //关闭excel
+            wbOut.Close(false, Type.Missing, Type.Missing);
+            wbsOut.Close();
+            excelOutput.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelOutput);
+
+            this.button2.Text = "导出";
+            this.button2.Enabled = true;
+            this.button1.Enabled = true;
+            this.textBox2.ReadOnly = false;
+            this.textBox3.ReadOnly = false;
+            this.textBox4.ReadOnly = false;
+            MessageBox.Show("统计完成！");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.maxDay = System.Threading.Thread.CurrentThread.CurrentUICulture.Calendar.GetDaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+            textBox4.Text = maxDay.ToString();
+
+            var startMoth = DateTime.Now.AddDays(-DateTime.Now.Day + 1).Date;
+            var stopMoth = startMoth.AddMonths(1).AddDays(-1).Date;
+
+            var tempDate = startMoth;
+            var today = DateTime.Now.Date;
+
+            while (tempDate <= stopMoth)
+            {
+                // Monday 从 国内星期天开始
+                if(tempDate.DayOfWeek == DayOfWeek.Sunday || tempDate.DayOfWeek == DayOfWeek.Saturday || tempDate>= today)
+                {
+                    if (textBox3.Text.Length != 0)
+                    {
+                        textBox3.Text = textBox3.Text + ",";
+                    }
+                    textBox3.Text = textBox3.Text + tempDate.Day.ToString();
+                }
+                tempDate = tempDate.AddDays(1);
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            this.maxDay = int.Parse(textBox4.Text);
+        }
+    }
+}
